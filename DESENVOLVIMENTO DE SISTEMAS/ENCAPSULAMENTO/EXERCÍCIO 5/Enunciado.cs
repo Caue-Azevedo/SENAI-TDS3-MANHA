450e6d884ed768de3ddcb4ee8c762bc455f76abf
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2025-04-17 11:27:19 -0300

    Create Enunciado.cs

---
+++ DESENVOLVIMENTO DE SISTEMAS/ENCAPSULAMENTO/EXERCÍCIO 5/Enunciado.cs
@@ -0,0 +1,17 @@
+** EXERCÍCIO 5: **
+Classe "Playlist" com Gerenciamento de Músicas
+
+Crie uma classe Playlist que:
+    Armazena uma lista privada de strings (nomes de músicas).
+    Tem uma propriedade DuracaoTotal (calculada com base em uma lógica interna).
+      
+Métodos públicos:
+    AdicionarMusica(string nome, int duracaoSegundos).
+    RemoverMusica(string nome).
+    ListarMusicas().
+
+Regras:
+    Não permitir músicas duplicadas.
+    A lista de músicas não pode ser modificada diretamente.  
+
+** **
