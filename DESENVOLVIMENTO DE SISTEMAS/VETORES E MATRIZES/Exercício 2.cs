85a11fea024e2fc1e0900a554a55a2ea9268b5d1
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2025-04-24 09:06:37 -0300

    Create Exercício 2.cs

---
+++ DESENVOLVIMENTO DE SISTEMAS/VETORES E MATRIZES/Exercício 2.cs
@@ -0,0 +1,63 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] salarios = new double[5];
+        Console.Clear();
+
+        LerSalarios(salarios);
+        AplicarAumento(salarios);
+        MostrarSalarios(salarios);
+    }
+
+    static void LerSalarios(double[] salarios)
+    {
+        for (int i = 0; i < salarios.Length; i++)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write($"Digite o {i + 1}º salário: ");
+                    string entrada = Console.ReadLine();
+
+                    if (double.TryParse(entrada, out double salario))
+                    {
+                        salarios[i] = salario;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Valor inválido. Digite um número válido.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao ler o salário: {ex.Message}");
+                }
+            }
+        }
+    }
+
+    static void AplicarAumento(double[] salarios)
+    {
+        for (int i = 0; i < salarios.Length; i++)
+        {
+            if (salarios[i] < 1000)
+            {
+                salarios[i] *= 1.1;
+            }
+        }
+    }
+
+    static void MostrarSalarios(double[] salarios)
+    {
+        Console.WriteLine("\nLista atualizada de salários:");
+        for (int i = 0; i < salarios.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}º salário: {salarios[i]:F2}");
+        }
+    }
+}
