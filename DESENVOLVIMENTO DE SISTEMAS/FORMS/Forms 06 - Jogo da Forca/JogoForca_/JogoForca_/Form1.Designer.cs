0be1d1a2ed092ae42064307052b10b9071486b02
Caue-Azevedo <cauecarlosdeazevedoguedes@gmail.com>
2025-05-06 09:25:52 -0300

    Add files via upload

---
+++ DESENVOLVIMENTO DE SISTEMAS/FORMS/Forms 06 - Jogo da Forca/JogoForca_/JogoForca_/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace JogoForca_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTentarLetra = new System.Windows.Forms.Button();
+            this.pnlPersonagem = new System.Windows.Forms.Panel();
+            this.btnNovoJogo = new System.Windows.Forms.Button();
+            this.lstLetrasTentadas = new System.Windows.Forms.ListBox();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.lblPalavraOculta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTentarLetra
+            // 
+            this.btnTentarLetra.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnTentarLetra.Location = new System.Drawing.Point(232, 312);
+            this.btnTentarLetra.Name = "btnTentarLetra";
+            this.btnTentarLetra.Size = new System.Drawing.Size(174, 58);
+            this.btnTentarLetra.TabIndex = 0;
+            this.btnTentarLetra.Text = "Tentar Letra";
+            this.btnTentarLetra.UseVisualStyleBackColor = false;
+            this.btnTentarLetra.Click += new System.EventHandler(this.btnTentarLetra_Click);
+            // 
+            // pnlPersonagem
+            // 
+            this.pnlPersonagem.Location = new System.Drawing.Point(41, 24);
+            this.pnlPersonagem.Name = "pnlPersonagem";
+            this.pnlPersonagem.Size = new System.Drawing.Size(199, 210);
+            this.pnlPersonagem.TabIndex = 1;
+            this.pnlPersonagem.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlPersonagem_Paint);
+            // 
+            // btnNovoJogo
+            // 
+            this.btnNovoJogo.BackColor = System.Drawing.Color.Wheat;
+            this.btnNovoJogo.Location = new System.Drawing.Point(559, 320);
+            this.btnNovoJogo.Name = "btnNovoJogo";
+            this.btnNovoJogo.Size = new System.Drawing.Size(210, 50);
+            this.btnNovoJogo.TabIndex = 2;
+            this.btnNovoJogo.Text = "Novo Jogo";
+            this.btnNovoJogo.UseVisualStyleBackColor = false;
+            this.btnNovoJogo.Click += new System.EventHandler(this.btnNovoJogo_Click);
+            // 
+            // lstLetrasTentadas
+            // 
+            this.lstLetrasTentadas.FormattingEnabled = true;
+            this.lstLetrasTentadas.ItemHeight = 20;
+            this.lstLetrasTentadas.Location = new System.Drawing.Point(748, 12);
+            this.lstLetrasTentadas.Name = "lstLetrasTentadas";
+            this.lstLetrasTentadas.Size = new System.Drawing.Size(40, 284);
+            this.lstLetrasTentadas.TabIndex = 3;
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(271, 280);
+            this.txtEntrada.MaxLength = 1;
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(100, 26);
+            this.txtEntrada.TabIndex = 4;
+            // 
+            // lblPalavraOculta
+            // 
+            this.lblPalavraOculta.AutoSize = true;
+            this.lblPalavraOculta.Location = new System.Drawing.Point(281, 214);
+            this.lblPalavraOculta.Name = "lblPalavraOculta";
+            this.lblPalavraOculta.Size = new System.Drawing.Size(111, 20);
+            this.lblPalavraOculta.TabIndex = 5;
+            this.lblPalavraOculta.Text = "Palavra Oculta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPalavraOculta);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.lstLetrasTentadas);
+            this.Controls.Add(this.btnNovoJogo);
+            this.Controls.Add(this.pnlPersonagem);
+            this.Controls.Add(this.btnTentarLetra);
+            this.Name = "Form1";
+            this.Text = "Jogo da Forca";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTentarLetra;
+        private System.Windows.Forms.Panel pnlPersonagem;
+        private System.Windows.Forms.Button btnNovoJogo;
+        private System.Windows.Forms.ListBox lstLetrasTentadas;
+        private System.Windows.Forms.TextBox txtEntrada;
+        private System.Windows.Forms.Label lblPalavraOculta;
+    }
+}
+
---
+++ DESENVOLVIMENTO DE SISTEMAS/FORMS/Forms 06 - Jogo da Forca/JogoForca_/JogoForca_/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JogoForca_
+{
+    public partial class Form1 : Form
+    {
+        string palavraSecreta;
+        char[] palavraOculta;
+        List<char> letrasTentadas = new List<char>();
+        int tentativasRestantes = 6;
+        List<string> palavras = new List<string>
+        {
+            "ABACAXI", "AVIÃO", "BANANA", "CACHORRO", "CADERNO", "CARRO",
+            "CELULAR", "COMPUTADOR", "ELEFANTE", "ESCOLA", "ESTRELA",
+            "GIRAFA", "JANELA", "LIVRO", "MONTANHA", "PIANO",
+            "PRAIA", "RELÓGIO", "SORVETE", "TARTARUGA"
+        };
+        bool jogoAtivo = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNovoJogo_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(palavras.Count);
+            palavraSecreta = palavras[index];
+
+            palavraOculta = new char[palavraSecreta.Length];
+            for (int i = 0; i < palavraSecreta.Length; i++)
+            {
+                palavraOculta[i] = '_';
+            }
+
+            letrasTentadas.Clear();
+            tentativasRestantes = 6;
+            lstLetrasTentadas.Items.Clear();
+            lblPalavraOculta.Text = ExibirPalavraOculta();
+            pnlPersonagem.Invalidate();
+            jogoAtivo = true;
+        }
+
+        private void btnTentarLetra_Click(object sender, EventArgs e)
+        {
+            if (!jogoAtivo)
+            {
+                MessageBox.Show("Clique em 'Novo Jogo' para começar.");
+                return;
+            }
+
+            string entrada = txtEntrada.Text.ToUpper();
+            txtEntrada.Clear();
+
+            if (entrada.Length != 1 || !char.IsLetter(entrada[0]))
+            {
+                MessageBox.Show("Digite apenas uma letra.");
+                return;
+            }
+
+            char letra = entrada[0];
+
+            if (letrasTentadas.Contains(letra))
+            {
+                MessageBox.Show("Letra já tentada.");
+                return;
+            }
+
+            letrasTentadas.Add(letra);
+            lstLetrasTentadas.Items.Add(letra);
+
+            if (palavraSecreta.Contains(letra))
+            {
+                for (int i = 0; i < palavraSecreta.Length; i++)
+                {
+                    if (palavraSecreta[i] == letra)
+                    {
+                        palavraOculta[i] = letra;
+
+                    }
+                }
+                lblPalavraOculta.Text = ExibirPalavraOculta();
+            }
+            else
+            {
+                tentativasRestantes--;
+                pnlPersonagem.Invalidate();
+
+                if (tentativasRestantes == 0)
+                {
+                    MessageBox.Show("Você perdeu! A palavra era: " + palavraSecreta);
+                    jogoAtivo = false;
+                }
+            }
+
+            if (!palavraOculta.Contains('_'))
+            {
+                MessageBox.Show("Parabéns! Você venceu!");
+                jogoAtivo = false;
+            }
+        }
+
+        private string ExibirPalavraOculta()
+        {
+            string palavraComEspacos = "";
+            foreach (char letra in palavraOculta)
+            {
+                palavraComEspacos += letra + " ";
+            }
+            return palavraComEspacos.Trim();
+        }
+
+        private void pnlPersonagem_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen caneta = new Pen(Color.Black, 2);
+
+            g.DrawLine(caneta, 10, 150, 100, 150);
+            g.DrawLine(caneta, 50, 150, 50, 20);
+            g.DrawLine(caneta, 50, 20, 100, 20);
+            g.DrawLine(caneta, 100, 20, 100, 40);
+
+            if (tentativasRestantes <= 5) g.DrawEllipse(caneta, 85, 40, 30, 30);
+            if (tentativasRestantes <= 4) g.DrawLine(caneta, 100, 70, 100, 110);
+            if (tentativasRestantes <= 3) g.DrawLine(caneta, 100, 80, 80, 100);
+            if (tentativasRestantes <= 2) g.DrawLine(caneta, 100, 80, 120, 100);
+            if (tentativasRestantes <= 1) g.DrawLine(caneta, 100, 110, 80, 130);
+            if (tentativasRestantes <= 0) g.DrawLine(caneta, 100, 110, 120, 130);
+        }
+    }
+}
